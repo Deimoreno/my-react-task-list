3e5a590a02d3b7031ba64cdb5c0d933b1afa5541
Deiber Mena Moreno <deibermmoreno@gmail.com>
2023-08-26 12:01:30 -0500

    actulizacion N°6

--- src/Pages/AboutUsPage.jsx
+++ src/Pages/AboutUsPage.jsx
@@ -4,9 +4,17 @@ function AboutUs() {
     return (
         <div>
             <h1> Sobre Nosotro </h1>
-            <p>
-
-            </p>
+            
+            <p>Nos complace presentarte la herramienta perfecta para mantenerte organizado y productivo en tu día a día. 
+        Nuestra aplicación de lista de tareas es intuitiva, fácil de usar y está diseñada para simplificar la gestión de tus actividades, 
+        sin importar cuán ocupado estés.</p>
+        <br />
+        <p>Con nuestra aplicación, podrás crear listas de tareas y agregar fácilmente nuevas tareas con solo unos pocos toques. 
+        Ya sea que estés planificando tu lista de compras, preparando un proyecto importante o simplemente recordando las tareas diarias, 
+        nuestra aplicación te ayudará a mantenerlo todo bajo control. </p>
+        <p>Nuestra interfaz limpia y moderna te permitirá navegar por tus listas de tareas de manera rápida y sencilla, y gracias a la sincronización en la nube, 
+        podrás acceder a tus tareas desde cualquier dispositivo en cualquier momento.
+        No pierdas más tiempo tratando de recordar tus pendientes.</p>
         </div>
     );
 }
